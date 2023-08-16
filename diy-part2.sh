cb15c94a7e71429f4884a57db69336ac492dd2e7
Stone <74167978+s71557@users.noreply.github.com>
2023-08-16 09:10:16 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,10 +11,10 @@
 #
 
 # 修改openwrt登陆地址,把下面的 10.0.0.1 修改成你想要的就可以了
-sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.100.1/192.168.11.1/g' package/base-files/files/bin/config_generate
 
 # 修改主机名字，把 iStore OS 修改你喜欢的就行（不能纯数字或者使用中文）
-sed -i 's/OpenWrt/StoneOS/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/iStore OS/g' package/base-files/files/bin/config_generate
 
 # ttyd 自动登录
 # sed -i "s?/bin/login?/usr/libexec/login.sh?g" ${GITHUB_WORKSPACE}/openwrt/package/feeds/packages/ttyd/files/ttyd.config
