2977f1ada7d70592290b2044414a063554690ba6
Stone <74167978+s71557@users.noreply.github.com>
2023-09-19 06:27:15 +0800

    更新 diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -41,12 +41,6 @@ svn export https://github.com/sbwml/luci-app-mosdns/trunk/luci-app-mosdns packag
 svn export https://github.com/sbwml/luci-app-mosdns/trunk/mosdns package/mosdns
 svn export https://github.com/sbwml/luci-app-mosdns/trunk/v2dat package/v2dat
 
-#Password
-svn export https://github.com/s71557/Lede-Packages/trunk/luci-app-passwall package/luci-app-passwall
-svn export https://github.com/s71557/Lede-Packages/trunk/luci-app-passwall2 package/luci-app-passwall2
-svn export https://github.com/s71557/Lede-Packages/trunk/openwrt-passwall-packages package/passwall
-
-
 echo "
 # 额外组件
 CONFIG_GRUB_IMAGES=y
@@ -63,31 +57,6 @@ CONFIG_VMDK_IMAGES=y
 
 # # mosdns
 # CONFIG_PACKAGE_luci-app-mosdns=y
-
-#password
-CONFIG_PACKAGE_luci-app-passwall2=y
-CONFIG_PACKAGE_luci-app-passwall=y
-CONFIG_PACKAGE_luci-app-passwall_Transparent_Proxy=y
-CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Brook=y
-CONFIG_PACKAGE_luci-app-passwall_INCLUDE_ChinaDNS_NG=y
-CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Haproxy=y
-CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Hysteria=y
-CONFIG_PACKAGE_luci-app-passwall_INCLUDE_IPv6_Nat=y
-CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Kcptun=y
-CONFIG_PACKAGE_luci-app-passwall_INCLUDE_NaiveProxy=y
-CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Shadowsocks_Libev_Client=y
-CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Shadowsocks_Libev_Server=y
-CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Shadowsocks_Rust_Client=y
-CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Shadowsocks_Rust_Server=y
-CONFIG_PACKAGE_luci-app-passwall_INCLUDE_ShadowsocksR_Libev_Client=y
-CONFIG_PACKAGE_luci-app-passwall_INCLUDE_ShadowsocksR_Libev_Server=y
-CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Simple_Obfs=y
-CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Trojan_GO=y
-CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Trojan_Plus=y
-CONFIG_PACKAGE_luci-app-passwall_INCLUDE_V2ray=y
-CONFIG_PACKAGE_luci-app-passwall_INCLUDE_V2ray_Plugin=y
-CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Xray=y
-CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Xray_Plugin=y
 
 " >> .config
 
