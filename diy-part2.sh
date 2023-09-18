c086142f79e116dadb2b8584c0aa84791c7bd169
Stone <74167978+s71557@users.noreply.github.com>
2023-09-18 15:12:44 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,9 +11,9 @@
 #
 
 # 修改openwrt登陆地址,把下面的 10.0.0.1 修改成你想要的就可以了
-sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.11.252/g' package/base-files/files/bin/config_generate
 # 修改 子网掩码
-sed -i 's/255.255.255.0/255.255.0.0/g' package/base-files/files/bin/config_generate
+#sed -i 's/255.255.255.0/255.255.0.0/g' package/base-files/files/bin/config_generate
 
 # 修改主机名字，把 iStore OS 修改你喜欢的就行（不能纯数字或者使用中文）
 # sed -i 's/OpenWrt/iStore OS/g' package/base-files/files/bin/config_generate
@@ -25,7 +25,7 @@ sed -i 's/255.255.255.0/255.255.0.0/g' package/base-files/files/bin/config_gener
 # sed -i 's/OpenWrt/OpenWrt/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 # 默认打开 WiFi
-sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+# sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 # OpenClash
 git clone https://github.com/vernesong/OpenClash package/OpenClash
