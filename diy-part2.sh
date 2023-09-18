642b407caf6429dfa6a2f751e66494e803ce7a79
Draco <draco.coder@gmail.com>
2023-09-18 15:32:04 +0800

    fix: 修复软件源

--- diy-part2.sh
+++ diy-part2.sh
@@ -27,14 +27,19 @@ sed -i 's/255.255.255.0/255.255.0.0/g' package/base-files/files/bin/config_gener
 # 默认打开 WiFi
 sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
-# OpenClash
-git clone https://github.com/vernesong/OpenClash package/OpenClash
+# openclash
+svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-openclash  package/luci-app-openclash
 
-# ADGuardHome
-git clone https://github.com/rufengsuixing/luci-app-adguardhome package/Adguardhome
+# adguardhome
+svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-adguardhome package/luci-app-adguardhome
+svn export https://github.com/kiddin9/openwrt-packages/trunk/adguardhome package/adguardhome
 
-# Mosdns
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+# mosdns
+rm -rf feeds/packages/net/mosdns
+rm -rf feeds/luci/applications/luci-app-mosdns
+svn export https://github.com/sbwml/luci-app-mosdns/trunk/luci-app-mosdns package/luci-app-mosdns
+svn export https://github.com/sbwml/luci-app-mosdns/trunk/mosdns package/mosdns
+svn export https://github.com/sbwml/luci-app-mosdns/trunk/v2dat package/v2dat
 
 echo "
 # 额外组件
